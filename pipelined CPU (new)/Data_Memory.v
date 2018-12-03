12ee7a3272ca3232e7944b2ed3d354382c0fde20
b05902023 <b05902023@ntu.edu.tw>
2018-12-03 14:55:12 +0800

    2018/12/3 14:55

--- pipelined CPU (new)/Data_Memory.v
+++ pipelined CPU (new)/Data_Memory.v
@@ -8,25 +8,23 @@ module Data_Memory
 	data_o
 );
 
-// Interface
-input				clk_i;
-input				MemWrite_i;
-input				MemRead_i;
-input   [31 : 0]	addr_i;
-input	[31 : 0]	data_i;
-output  [31 : 0]	data_o;
+input           clk_i;
+input           MemWrite_i;
+input           MemRead_i;
+input  [31 : 0]	addr_i;
+input  [31 : 0]	data_i;
+output [31 : 0]	data_o;
 
-// Data memory
-reg		[7 : 0]	    memory [0 : 31];
+reg    [7 : 0]  memory [0 : 31];
 
-assign	data_o = (MemRead_i == 1'b1)? {((memory[addr_i][7] == 1'b0)? 24'b0 : 24'b1) , memory[addr_i]} : 32'b0;
+assign data_o = (MemRead_i == 1'b1)? {((memory[addr_i][7] == 1'b0)? 24'b0 : 24'b1) , memory[addr_i]} : 32'b0;
 
 always @(posedge clk_i)
+begin
+	if (MemWrite_i == 1'b1)
 	begin
-		if (MemWrite_i == 1'b1)
-			begin
-				memory[addr_i][7 : 0] <= data_i[7 : 0];
-			end
+		memory[addr_i][7 : 0] <= data_i[7 : 0];
 	end
+end
 
 endmodule
