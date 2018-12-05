a993fc61fe63ddde9d6689e0d3a22a2ff20cfdd9
b05902023 <b05902023@ntu.edu.tw>
2018-12-05 15:46:45 +0800

    2018/12/5 15:46

---
+++ pipelined CPU (forward)/Data_Memory.v
@@ -0,0 +1,30 @@
+module Data_Memory
+(
+	clk_i,
+	MemWrite_i,
+	MemRead_i,
+	addr_i,
+	data_i,
+	data_o
+);
+
+input           clk_i;
+input           MemWrite_i;
+input           MemRead_i;
+input  [31 : 0]	addr_i;
+input  [31 : 0]	data_i;
+output [31 : 0]	data_o;
+
+reg    [7 : 0]  memory [0 : 31];
+
+assign data_o = (MemRead_i == 1'b1)? {((memory[addr_i][7] == 1'b0)? 24'b0 : 24'b1) , memory[addr_i]} : 32'b0;
+
+always @(posedge clk_i)
+begin
+	if (MemWrite_i == 1'b1)
+	begin
+		memory[addr_i][7 : 0] <= data_i[7 : 0];
+	end
+end
+
+endmodule
---
+++ pipelined CPU (forward)/IF_ID.v
@@ -0,0 +1,28 @@
+module IF_ID
+(
+	clk_i,
+	PC_i,
+	PC_o,
+	inst_i,
+	inst_o
+);
+
+input           clk_i;
+input  [31 : 0] PC_i;
+output [31 : 0] PC_o;
+input  [31 : 0] inst_i;
+output [31 : 0] inst_o;
+
+reg    [31 : 0] PC_reg;
+reg    [31 : 0] inst_reg;
+   
+assign PC_o = PC_reg;
+assign inst_o = inst_reg;
+ 
+always @(posedge clk_i)
+begin
+	PC_reg <= PC_i;
+	inst_reg <= inst_i;
+end
+   
+endmodule 
