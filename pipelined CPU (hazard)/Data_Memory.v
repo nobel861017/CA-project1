a538d51f2af9cbeaf2b9581de743b5a92365227f
b05902023 <b05902023@ntu.edu.tw>
2018-12-06 01:29:23 +0800

    2018/12/6 1:39

--- pipelined CPU (hazard)/Data_Memory.v
+++ pipelined CPU (hazard)/Data_Memory.v
@@ -17,13 +17,16 @@ output [31 : 0]	data_o;
 
 reg    [7 : 0]  memory [0 : 31];
 
-assign data_o = (MemRead_i == 1'b1)? {((memory[addr_i][7] == 1'b0)? 24'b0 : 24'b1) , memory[addr_i]} : 32'b0;
+assign data_o = (MemRead_i == 1'b1)? {memory[addr_i + 3] , memory[addr_i + 2] , memory[addr_i + 1] , memory[addr_i]} : 32'b0;
 
 always @(posedge clk_i)
 begin
 	if (MemWrite_i == 1'b1)
 	begin
-		memory[addr_i][7 : 0] <= data_i[7 : 0];
+		memory[addr_i] <= data_i[7 : 0];
+                memory[addr_i + 1] <= data_i[15 : 8];
+                memory[addr_i + 2] <= data_i[23 : 16];
+                memory[addr_i + 3] <= data_i[31 : 24];
 	end
 end
 
